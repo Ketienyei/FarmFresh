d3d4621ccaa0eb798d73fd7ba37176837f2aab04
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-03-07 23:11:51 +0300

    database

--- database/migrations/2023_03_07_184949_create_permissions_table.php
+++ database/migrations/2023_03_07_184949_create_permissions_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->text('name');
-            $table->text('slug');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('permissions');
     }
 };
